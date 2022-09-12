32ec5f3be3eedc2c196caef6c0a5c69e54a3a10c
Gabriel Santos <gab.tec97@gmail.com>
2022-09-12 13:26:31 -0300

    First commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <v-container class="grey lighten-5">
+      <v-row no-gutters>
+        <v-col cols="sm"></v-col>
+        <v-col cols="sm">
+          <v-card class="pa-2">
+            <h1>Login page</h1>
+          </v-card>
+        </v-col>
+        <v-col cols="sm"> </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
